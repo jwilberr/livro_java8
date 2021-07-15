634f84d6270adf7f739358ff7bff22ebecaba443
Wilber <jwilber1972@gmail.com>
2021-07-15 13:59:30 -0300

    projeto criado

---
+++ src/livro_java08/Aula_2_6_1_ClasseDataInputStream.java
@@ -0,0 +1,18 @@
+package livro_java08;
+
+import java.io.DataInputStream;
+
+public class Aula_2_6_1_ClasseDataInputStream {
+
+	public static void main(String[] args) {
+		
+		String s;
+		float largura, comprimento, area, perimetro;
+			
+		DataInputStream dado;
+		
+		
+
+	}
+
+}
